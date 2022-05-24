167633b593b85b9176bfc0038f4f8c28e5cfae14
Florian Förg <florian-foerg@web.de>
2022-05-24 15:53:59 +0200

    Added pictures to RestaurantListComponents

--- client/src/components/RestaurantCardComp.js
+++ client/src/components/RestaurantCardComp.js
@@ -4,6 +4,7 @@ function RestaurantCardComp(props){
     <div class="card w-100 mt-3">
             <div class="card-body">
               <h5 class="card-title">{props.name}</h5>
+              <img class="card-img-top" src={props.src} alt="Card image cap"></img>
               <p class="card-text">Cuisine: {props.cuisine}, Rating: {props.rating} / 10, Location: {props.location}</p>
               <a href="#" class="btn btn-outline-dark">Anzeigen</a>
               <a href="#" class="btn btn-outline-dark">Reservieren</a>
--- client/src/components/RestaurantListComp.js
+++ client/src/components/RestaurantListComp.js
@@ -7,20 +7,20 @@ class RestaurantListComp extends Component {
     return (
       <div>
 
-        <RestaurantCardComp name="PizzaHut"  cuisine="Italian" rating="5.0" location = "Schwabing"/>
-        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt"/>
-        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt"/>
-        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt"/>
-        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt"/>
-        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt"/>
-        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt"/>
-        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt"/>
-        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt"/>
-        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt"/>
-        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt"/>
-        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt"/>
-        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt"/>
-        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt"/>
+        <RestaurantCardComp name="PizzaHut"  cuisine="Italian" rating="5.0" location = "Schwabing" src="https://www.docohotel.com/media/163371/do-co-hotel-muenchen-restaurant-2-kopie.jpg"/>
+        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt" src="https://www.mux.de/images/1500x1200z/object/70/1156099970/mcdonalds-1.JPG"/>
+        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt" src="https://www.mux.de/images/1500x1200z/object/70/1156099970/mcdonalds-1.JPG"/>
+        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt" src="https://www.mux.de/images/1500x1200z/object/70/1156099970/mcdonalds-1.JPG"/>
+        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt" src="https://www.mux.de/images/1500x1200z/object/70/1156099970/mcdonalds-1.JPG"/>
+        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt" src="https://www.mux.de/images/1500x1200z/object/70/1156099970/mcdonalds-1.JPG"/>
+        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt" src="https://www.mux.de/images/1500x1200z/object/70/1156099970/mcdonalds-1.JPG"/>
+        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt" src="https://www.mux.de/images/1500x1200z/object/70/1156099970/mcdonalds-1.JPG"/>
+        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt" src="https://www.mux.de/images/1500x1200z/object/70/1156099970/mcdonalds-1.JPG"/>
+        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt" src="https://www.mux.de/images/1500x1200z/object/70/1156099970/mcdonalds-1.JPG"/>
+        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt" src="https://www.mux.de/images/1500x1200z/object/70/1156099970/mcdonalds-1.JPG"/>
+        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt" src="https://www.mux.de/images/1500x1200z/object/70/1156099970/mcdonalds-1.JPG"/>
+        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt" src="https://www.mux.de/images/1500x1200z/object/70/1156099970/mcdonalds-1.JPG"/>
+        <RestaurantCardComp name="McDonalds"  cuisine="American" rating="2.3" location = "Maxvorstadt" src="https://www.mux.de/images/1500x1200z/object/70/1156099970/mcdonalds-1.JPG"/>
 
       </div>
     );
